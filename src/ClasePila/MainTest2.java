2db87c6316fb9779125ea2649c20b04b60acbd1d
Andrewkxz <jaidera.melor@uqvirtual.edu.co>
2025-10-02 21:09:59 -0500

    corrección de error e implementación del Main

---
+++ src/ClasePila/MainTest2.java
@@ -0,0 +1,41 @@
+package ClasePila;
+
+public class MainTest2 {
+    public static void main(String[] args) {
+        Pila<Double> lista1 = new Pila<>();
+
+        /**
+        lista.agregarPrimero("una lista");
+        lista.agregarPrimero("desde");
+        lista.agregarPrimero("a todos");
+        lista.agregarPrimero("Hola");
+        **/
+
+        /** 
+        lista.agregarUltimo("Hola");
+        lista.agregarUltimo("a todos");
+        lista.agregarUltimo("desde");
+        lista.agregarUltimo("una lista");
+        **/
+
+        /** 
+        lista.agregarPosicion("sapa", 0);
+        lista.agregarPosicion("a todos", 1);    
+        lista.agregarPosicion("Hola", 2);  
+        lista.agregarPosicion("una lista", 3);
+        **/
+
+        lista1.agregarOrdenNatural(1.5);
+        lista1.agregarOrdenNatural(2.3);
+        lista1.agregarOrdenNatural(1.1);
+        lista1.agregarOrdenNatural(3.7);
+
+        System.out.println("Recorrido con for-each:");
+        for(Double num : lista1){
+            System.out.println(num);
+        }
+        
+
+        //lista1.mostrar();    
+    }
+}
--- src/ListaDobleCircular/IteratorListaDobleCircular.java
+++ src/ListaDobleCircular/IteratorListaDobleCircular.java
@@ -3,20 +3,25 @@ import java.util.Iterator;
 
 public class IteratorListaDobleCircular <T extends Comparable <T>> implements Iterator<T> {
     private NodoDoble<T> actual;
+    private NodoDoble<T> primero; // Nodo inicial para detectar el ciclo
+    private boolean recorridoCompleto; // Indicador para saber si hemos completado un ciclo
 
     public IteratorListaDobleCircular(ListaDobleCircular<T> lista){
-        this.actual = lista.getPrimero();
+        this.primero = lista.getPrimero();
+        this.actual = primero;
+        this.recorridoCompleto = false;
     }
 
     @Override
     public boolean hasNext() {
-        return actual != null;
+        return actual != null && (!recorridoCompleto || actual != primero);
     }
 
     @Override
     public T next() {
         T dato = actual.getDato(); //Obtenemos el dato del nodo actual.
         actual = actual.getProximo(); //Avanzamos al siguiente nodo.
+        recorridoCompleto = true; // Marcamos que hemos avanzado al menos una vez
         return dato; //Devolvemos el dato del nodo actual.
     }
     
